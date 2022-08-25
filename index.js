c41d718f6aa78b904a898aaea6648f463b0edf54
AxterDoesCode <74308086+AxterDoesCode@users.noreply.github.com>
2022-08-25 14:17:06 +0100

    Project completion
    Project has been adjusted to handle edge cases such as copy pasting data.
    Aswell as not being able to submit when the charactercount > 140.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-document.getElementById("commentBox").addEventListener("submit", handleSubmit)
+const commentBox = document.getElementById("commentBox")
+const commentInput = document.querySelector("textarea")
+const characterCount = document.getElementById("character-count")
+commentBox.addEventListener("submit", handleSubmit)
 
 function handleSubmit(e){
     e.preventDefault()
-    let username=document.getElementById("username").value
-    let comment=document.getElementById("comment").value
-    document.getElementById("comments").innerHTML +=`
-        <h1>${username}</h1>
-        <p>${comment}</p>
-    `
+    let username=document.getElementById("username-input").value
+    let comment=document.getElementById("comment-input").value
+    if(username&&comment&&commentInput.value.length <= 140)
+        {document.getElementById("comments").innerHTML +=`
+            <div class="comment-container">
+                <p>${username}: ${comment}</p>
+            </div>
+        `
+        commentBox.reset()
+    }
+    
 }
+
+commentInput.addEventListener("input", ({currentTarget: target}) => {
+    const maxLength = 140
+    const currentLength = target.value.length
+
+    characterCount.value = `${currentLength}/140`
+    currentLength > maxLength ? target.classList="red" : target.classList=""
+})
